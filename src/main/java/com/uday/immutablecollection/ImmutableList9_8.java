e368a5d56f993a63dd2539f76cac315a494ccd0d
udayk <udaykumar1113@gmail.com>
2019-01-03 08:56:43 -0500

    created immutable list using java 1.9

---
+++ src/main/java/com/uday/immutablecollection/ImmutableList9_8.java
@@ -0,0 +1,10 @@
+package com.uday.immutablecollection;
+
+import java.util.List;
+
+public class ImmutableList9_8 {
+    public static void main(String args[]){
+        List<String> immutableList=List.of("A","B","C","D");
+        System.out.println(immutableList);
+    }
+}
